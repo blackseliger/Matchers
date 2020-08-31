9b2e3c36e60f4214dbf470eaa574daad2fb8c9c3
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-31 10:11:09 +0300

    re-write tests

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,13 +1,26 @@
 import lifeCheck from '../app';
 
 describe('check lives profiles and sort them', () => {
-  test('should be from max lives to min', () => {
+  test('sorted array from min to max', () => {
     const expected = [
       { name: 'маг', health: 100 },
       { name: 'лучник', health: 80 },
       { name: 'мечник', health: 10 },
     ];
 
+    const received = lifeCheck([
+      { name: 'мечник', health: 10 },
+      { name: 'лучник', health: 80 },
+      { name: 'маг', health: 100 },
+    ]);
+    expect(received).toEqual(expected);
+  });
+  test('from max to max, leave the array as it is', () => {
+    const expected = [
+      { name: 'маг', health: 100 },
+      { name: 'лучник', health: 80 },
+      { name: 'мечник', health: 10 },
+    ];
     const received = lifeCheck([
       { name: 'маг', health: 100 },
       { name: 'лучник', health: 80 },
@@ -15,7 +28,7 @@ describe('check lives profiles and sort them', () => {
     ]);
     expect(received).toEqual(expected);
   });
-  test('the two variables must be equal and filtered in the list', () => {
+  test('from min to max, but should exist 2 equal elements', () => {
     const expected = [
       { name: 'мечник', health: 95 },
       { name: 'маг', health: 80 },
@@ -27,6 +40,22 @@ describe('check lives profiles and sort them', () => {
       { name: 'лучник', health: 80 },
       { name: 'мечник', health: 95 },
     ]);
+    expect(received).toEqual(expected);
+  });
+  test('array with two elements', () => {
+    const expected = [
+      { name: 'маг', health: 90 },
+      { name: 'лучник', health: 80 },
+    ];
+    const received = lifeCheck([
+      { name: 'маг', health: 90 },
+      { name: 'лучник', health: 80 },
+    ]);
+    expect(received).toEqual(expected);
+  });
+  test('empty array', () => {
+    const expected = [];
+    const received = lifeCheck([]);
     expect(received).toEqual(expected);
   });
 });
